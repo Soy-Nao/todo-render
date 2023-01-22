ea321ccefb64ac2a9841ba276ca66b5e88476cd3
Nao <loscanalesdenao@gmail.com>
2023-01-22 10:52:07 -0500

    autenticacion detoken

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,23 +1,34 @@
-const jwt  = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const authMiddleware = ( req, res, next ) => {
-    let { authorization: token } = req.headers;
-    token = token?.replace("Bearer ", "");
+const authMiddleware = (req, res, next) => {
+  let { authorization: token } = req.headers;
+  token = token?.replace("Bearer ", "");
+  if (token) {
     console.log(token);
-    jwt.verify(token, process.env.JWT_SECRET, {algorithms: "HS512"},
-    (err, decoded) => {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: "HS512" },
+      (err, decoded) => {
         if (err) {
-            res.status(400).json({
-                error: "invalid token",
-                message: "El token no es valido o ya expiró, envia un token correcto"
-            });
-        }else {
-            console.log(decoded)
-            next();
+          res.status(400).json({
+            error: "invalid token",
+            message:
+              "El token no es valido o ya expiró, envia un token correcto",
+          });
+        } else {
+          console.log(decoded);
+          next();
         }
-    })
-    
+      }
+    );
+  } else {
+    res.status(400).json({
+      error: "no token provined",
+      message: "No se está enviando un token de autenticacion",
+    });
+  }
 };
 
 module.exports = authMiddleware;
